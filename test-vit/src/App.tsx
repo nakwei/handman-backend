afbfe946efffdea2a8563d4160d2079bbdadde44
Nathan Kwei <kweinathan@gmail.com>
2024-02-02 20:30:18 -0500

    Completed assignment from README page
    added the circle with dimensions and centered
    added the background color javascript effects

--- test-vit/src/App.tsx
+++ test-vit/src/App.tsx
@@ -3,23 +3,50 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./index.css";
 
+let colorIndex = 0;
+
+function changeColors() {
+  const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'white'];
+  document.body.style.backgroundColor = colors[colorIndex];
+  colorIndex = (colorIndex+1) % colors.length;
+
+}
+// function lightenBG() {
+//   document.body.style.backgroundColor.opacit
+
+// }
+
 function App() {
   const [count, setCount] = useState(0);
+  
 
   return (
     <>
-      <h1 className="text-purple-800 bg-[#fefefe] text-3xl font-bold test-red-500 underline text-center">
+    <div>
+      <h1 className="text-purple-800 bg-[#fefefe] text-3xl font-bold 
+      test-red-500 underline text-center">
         This is a testing start page!
       </h1>
+      <div className="min-h-screen grid place-content-center">
+        <div className="bg-emerald-500 h-[102px] w-[102px] rounded-full 
+        hover: document.body.style.background.lighten"
+            onClick={changeColors} 
+            
+            ></div>
+      </div>
+
+      
 
       <div>{count}</div>
-
+      
       <button onClick={() => setCount((last) => last + 1)}>Up</button>
       <button onClick={() => setCount((last) => last - 1)}>Down</button>
 
       <img src={viteLogo} />
       <img src={reactLogo} />
+      </div>
     </>
+    
   );
 }
 
