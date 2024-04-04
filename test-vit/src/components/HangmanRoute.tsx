6be51e6caf4648513f2320f65f6be65bbe568486
nakwei <kweinathan@gmail.com>
2024-04-04 12:13:36 -0400

    body and formatting (#3)
    * body and formatting
    
    * refactored body parts and state
    
    * play again button
    
    * last few changes to winlose
    
    * finishing hangman
    
    * replace letter set with regular expression
    
    * add vs code settings
    
    * use ternaries and string type unions
    
    ---------
    
    Co-authored-by: Adrian Adkison <aadkison@grindfoundry.com>

--- test-vit/src/components/HangmanRoute.tsx
+++ test-vit/src/components/HangmanRoute.tsx
@@ -1,26 +1,125 @@
 import { useEffect, useState } from "react";
+import { BodyFull } from "./BodyFull.tsx";
+import { WinLose } from "./WinLose.tsx";
+import { Noose } from "./Noose.tsx";
+import { LetterSpaces } from "./LetterSpaces.tsx";
+import { GameSate } from "./types.ts";
 
-export const words = ["car", "tree", "bush"];
-export const allowedWrongCount = 5;
+export const words = [
+  "car",
+  "tree",
+  "bush",
+  "hello",
+  "apple",
+  "stations",
+  "taxi",
+  "constellation",
+  "telescope",
+  "pants",
+  "eyeball",
+  "sandwich",
+  "shoes",
+  "market",
+  "phone",
+  "camera",
+  "chair",
+  "backyard",
+  "cat",
+  "sea",
+];
+
+const alphanumericCharacter = /[a-z]/i;
+
+const LOSE_COUNT = 6;
+
+// to fix:
+// ensure that same letter Caps and no caps count as one letter
+// ensure that caps letter count as a guessed letter valid
+// problem rn: why doesn't captial letter guess count as a right answer. rn, stopping it from being a right ansewr.
 export const HangmanRoute = () => {
   const [guessed, setGuessed] = useState<string[]>([]);
-  const [word] = useState(() => {
-    Math.round(Math.random() * 2);
+  const [word, setNextWord] = useState(() => {
+    return words[Math.round(Math.random() * words.length)];
   });
 
+  const wrongGuesses = guessed.filter(
+    (char) => !word.includes(char.toLowerCase())
+  );
+  const wrongGuessSet = new Set(wrongGuesses);
+  const correctGuesses = new Set(
+    guessed.filter((char) => word.includes(char.toLowerCase()))
+  );
+  const hasGuessedWord = [...word].every((char) =>
+    guessed.includes(char.toLowerCase())
+  );
+  const gameState: GameSate =
+    wrongGuessSet.size === LOSE_COUNT
+      ? "lose"
+      : hasGuessedWord
+      ? "win"
+      : "playing";
+
+  const restart = () => {
+    setNextWord(() => words[Math.round(Math.random() * words.length)]);
+    setGuessed(() => []);
+  };
+
+  const gameStateBool = gameState == "win" || gameState == "lose";
+
+  useEffect(() => {
+    gameState === "lose" && (document.title = "You Lose!");
+    gameState === "win" && (document.title = "You Win!");
+  }, [gameState]);
+
   return (
-    <div className="p-20">
-      <input
-        className="border border-red-600 outline-4 outline-red-600"
-        type="text"
-        value={""}
-        onChange={(e) =>
-          setGuessed((last) => {
-            return [...last, e.target.value];
-          })
-        }
-      />
-      <div>{JSON.stringify(guessed, null, 2)}</div>
+    <div>
+      <Noose />
+      <div className="flex justify-center items-stretch">
+        <div className="absolute top-5">
+          <WinLose gameState={gameState} restart={restart}></WinLose>
+        </div>
+      </div>
+
+      <div className="absolute left-1/2 -translate-x-1/2 top-40">
+        <BodyFull wrongGuessCount={wrongGuessSet.size} />
+      </div>
+
+      <div className="absolute left-1/2 -translate-x-1/2 bottom-1/3">
+        <LetterSpaces
+          letters={word}
+          correctLetters={correctGuesses}
+          gameState={gameState}
+        />
+      </div>
+
+      <div className="absolute left-1/2 -translate-x-1/2 top-3/4">
+        <input
+          className="border border-red-600 outline-4 outline-red-600"
+          type="text"
+          value={""}
+          disabled={gameStateBool}
+          onChange={(e) => {
+            if (alphanumericCharacter.test(e.target.value)) {
+              setGuessed((last) => {
+                return [...last, e.target.value];
+              });
+            }
+          }}
+        />
+      </div>
+
+      <div className="text-gray-500">word-test: {word}</div>
+      <div
+        className="h-[5rem] w-[30rem] border-black border p-2 text-red-800
+        absolute left-1/2 -translate-x-1/2 bottom-12 flex gap-x-2"
+      >
+        {wrongGuesses.map((guess, index) => (
+          <div key={index}>
+            {guess}
+            {index < wrongGuesses.length - 1 ? "," : ""}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ test-vit/src/components/WinLose.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react";
+
+export const WinLose: FC<{gameState: string; restart: ()=>void}> = ({gameState, restart}) => {
+    return (
+    <div>
+        {gameState === "lose" && (
+            <div className="flex gap-10  items-center">
+                <div>You've Lost</div>
+                <button className="outline bg-red-300 hover:bg-red-400" onClick={restart}>Play Again</button>
+            </div> 
+        )
+        } {(gameState=="win") && (
+            <div className="flex gap-10 items-center">
+                <div>You've Won </div>
+                <button className="outline bg-red-300 hover:bg-red-400 p-1" onClick={restart}>Play Again</button>
+            </div>
+            
+            )
+        }
+        </div>
+    
+    )
+    }
+        
+
+    // )
+        
+    
